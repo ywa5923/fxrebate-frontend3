930bb797f88e8813a709e94ea6592d2c40f5e2bd
Ion Ivan <ivanion10@gmail.com>
2025-02-12 22:02:59 +0200

    Reinitialization npx shadcn@latest init

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,4 @@
-"use client"
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {

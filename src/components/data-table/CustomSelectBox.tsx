dac23a2b986759adc05b96bd053b92237f5aa7be
Ion Ivan <ivanion10@gmail.com>
2025-02-22 03:12:12 +0200

    CustomSelectBox optimized for rapid selection

--- src/components/data-table/CustomSelectBox.tsx
+++ src/components/data-table/CustomSelectBox.tsx
@@ -1,37 +1,34 @@
 "use client";
+
+import { useState, useEffect } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { Checkbox } from "@/components/ui/checkbox";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { useState } from "react";
 import { Button } from "../ui/button";
-import { FilterField } from "@/types";
-import { FilterOption } from "@/types";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import {RatingComponent} from "./RatingComponent";
-import { useEffect } from "react";
+import { RatingComponent } from "./RatingComponent";
+import { FilterField, FilterOption } from "@/types";
+
 export function CustomSelectBox({
-  filter: { options, name, field,type,expanded},
+  filter: { options, name, field, type, expanded },
   selected,
-  
 }: {
   filter: FilterField;
   selected?: string[] | null;
-  
 }) {
   const [open, setOpen] = useState(false);
- 
   const [checkedItems, setCheckedItems] = useState<string[]>([]);
-
 
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const params = new URLSearchParams(searchParams);
   const { replace } = useRouter();
+  const params = new URLSearchParams(searchParams);
 
   useEffect(() => {
-    setCheckedItems([...(selected || [])]); // Update checkedItems with selected || []);
+    setCheckedItems(selected ? [...selected] : []);
   }, [selected]);
-  const onCheckedChange = (
+
+  const handleCheckedChange = (
     checked: boolean | string,
     value: string,
     field: string,
@@ -41,20 +38,18 @@ export function CustomSelectBox({
 
     const paramsArray = new Set(params.get(field)?.split(",") || []);
     if (checked) {
-          if (type === "radio") paramsArray.clear();
-        paramsArray.add(value);       
+      if (type === "radio") paramsArray.clear();
+      paramsArray.add(value);
     } else {
       paramsArray.delete(value);
-      if (paramsArray.size == 0) {
+      if (!paramsArray.size) {
         params.delete(field);
         replace(`${pathname}?${params.toString()}`);
         return;
       }
-      
     }
     params.set(field, Array.from(paramsArray).join(","));
     replace(`${pathname}?${params}`);
-    
   };
 
   return (
@@ -64,92 +59,68 @@ export function CustomSelectBox({
         className="mt-2 w-full"
         {...(!expanded && { onClick: () => setOpen(!open) })}
       >
-        {name} {checkedItems?.length ? `(${checkedItems.length})` : ""}
+        {name} {checkedItems.length ? `(${checkedItems.length})` : ""}
       </Button>
-      <ScrollArea className="max-h-[200px]  rounded-md border scrollbar-thin scrollbar overflow-y-auto">
-       
-         
-          {(open || expanded) && (
-            <div>
+      <ScrollArea className="max-h-[200px] rounded-md border scrollbar-thin overflow-y-auto">
+        {(open || expanded) && (
+          <div>
             {type === "radio" || type === "rating" ? (
-              // Render RadioGroup if type is "radio"
               <RadioGroup
-                onValueChange={(value) => onCheckedChange(true, value, field, type)}
+                onValueChange={(value) => handleCheckedChange(true, value, field, type)}
                 defaultValue={selected?.[0] ?? ""}
               >
-                {options?.map((option: FilterOption, index) => (
-                  <div className="flex items-center space-x-2" key={index}>
-                    <RadioGroupItem
-                      id={`${option.value}`}
-                      value={option.value}
-                    />
-                    <label
-                      htmlFor={`${option.value}`}
-                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    >
+                {options?.map((option) => (
+                  <div className="flex items-center space-x-2" key={option.value}>
+                    <RadioGroupItem id={option.value} value={option.value} />
+                    <label htmlFor={option.value} className="text-sm font-medium">
                       {option.name}
                     </label>
-                    {type === "rating" && <RatingComponent key={index+"ds"}/>}
+                    {type === "rating" && <RatingComponent />}
                   </div>
-                   
                 ))}
               </RadioGroup>
             ) : (
               // Render Checkboxes if type is "checkbox"
-              options?.map((option: FilterOption, index) => {
-                const checked = checkedItems?.includes(option.value) ?? false;
-              
+              options?.map((option) => {
+                const checked = checkedItems.includes(option.value);
                 return (
                   <div className="flex items-center space-x-2 p-1" key={option.value}>
                     <Checkbox
-                      id={`${option.value}`}
+                      id={option.value}
                       defaultChecked={checked}
-                      onCheckedChange={(checked) =>
-                        onCheckedChange(checked, option.value, field, type)
-                      }
+                      onCheckedChange={(checked) => handleCheckedChange(checked, option.value, field, type)}
                     />
-                   <label htmlFor={option.value} className="text-sm font-medium">
+                    <label htmlFor={option.value} className="text-sm font-medium">
                       {option.name}
                     </label>
                   </div>
                 );
               })
-
             )}
-            
-            </div>
-          )}
-        
-        </ScrollArea>
-        <div className="flex flex-wrap flex-col gap-1 m-2">
-          
-        {!expanded && checkedItems?.map((item, index) => (
-             
-              <div
-              key={index}
+          </div>
+        )}
+      </ScrollArea>
+      <div className="flex flex-wrap flex-col gap-1 m-2">
+        {!expanded &&
+          checkedItems.map((item) => (
+            <div
+              key={item}
               className="flex items-center justify-between rounded-full bg-blue-100 px-2 py-0.5 text-sm font-medium text-blue-800"
-             >
+            >
               {options?.find((option) => option.value === item)?.name}
               <span
                 onClick={() => {
-                  onCheckedChange(false, item, field, type);
-                  setCheckedItems((prev) => {
-                    // If prev is null, default to an empty array
-                    const prevItems = Array.isArray(prev) ? prev : [];
-                    return prevItems.filter((i) => i !== item);
-                  });
-                  setOpen(false)
-                 }}
+                  handleCheckedChange(false, item, field, type);
+                  setCheckedItems((prev) => prev.filter((i) => i !== item));
+                  setOpen(false);
+                }}
                 className="font-bold cursor-pointer ml-2"
               >
                 ×
               </span>
             </div>
-
-            ))
-          }
-        </div>
-      
+          ))}
+      </div>
     </div>
   );
 }
